7fa6d02e37832273f58d0a9195c8b78d1d8d2b4b
behemoth-hunglt <hunglt@behemoth.vn>
2025-04-03 06:27:56 +0700

    Include .output in Git tracking

--- server/plugins/mongoose.ts
+++ server/plugins/mongoose.ts
@@ -2,18 +2,33 @@ import mongoose from 'mongoose'
 import { defineNitroPlugin } from 'nitropack/runtime/plugin'
 
 export default defineNitroPlugin(async (nitroApp) => {
-    // Chỉ kết nối MongoDB trong môi trường development
-    if (process.env.NODE_ENV !== 'development') {
+  const config = useRuntimeConfig()
+
+  try {
+    // Kiểm tra nếu đã kết nối thì không kết nối lại
+    if (mongoose.connection.readyState === 1) {
       return
     }
-  const config = useRuntimeConfig()
-  
-  try {
+
     mongoose.set('strictQuery', true)
     await mongoose.connect(config.MONGODB_URI)
     console.log('Connected to MongoDB successfully')
+    console.log('MONGODB_URI:', process.env.NODE_ENV)
+
+    // Đóng kết nối khi build xong
+    if (process.env.NODE_ENV === 'prerender') {
+      // Đóng kết nối sau 5 giây
+      setTimeout(async () => {
+        try {
+          await mongoose.connection.close()
+          console.log('MongoDB connection closed')
+        } catch (error) {
+          console.error('Error closing MongoDB connection:', error)
+        }
+      }, 30000)
+    }
   } catch (error) {
     console.error('Error connecting to MongoDB:', error)
     throw error
   }
-}) 
+})
